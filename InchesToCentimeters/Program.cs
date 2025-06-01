628e31774dd13655b5a0adf6079f69182ecb8869
Florentino Aguirre <florentinoaguirre@lewisu.edu>
2025-06-01 18:18:51 -0500

    Commit

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double CM_PER_INCH = 2.54;
+            Console.Write("Enter measurement in inches: ");
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double inches))
+            {
+                double centimeters = inches * CM_PER_INCH;
+                Console.WriteLine("{0} inches is {1} centimeters.", inches, centimeters);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
