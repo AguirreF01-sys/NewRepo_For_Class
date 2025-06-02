a3c4c1a044b412d0e57e94bd0c17968c35dac144
Florentino Aguirre <florentinoaguirre@lewisu.edu>
2025-06-01 20:22:53 -0500

    Update

--- Week1 Lab Assignments/InchesToCentimeters/Program.cs
+++ Week1 Lab Assignments/InchesToCentimeters/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * * Florentino Aguirre
+ * CPSC 23000 .NET Programming
+ * Week 1 Lab Assignment
+ * Inch to Centimeters Calculator 
+ * Program will take user input to represent inch value and display the conversion amount for centimeters. 
+ */
+
+using System;
 
 namespace InchesToCentimeters
 {
@@ -6,12 +14,12 @@ namespace InchesToCentimeters
     {
         static void Main(string[] args)
         {
-            const double CM_PER_INCH = 2.54;
+            const double cmPerInch = 2.54;
             Console.Write("Enter measurement in inches: ");
             string input = Console.ReadLine();
             if (double.TryParse(input, out double inches))
             {
-                double centimeters = inches * CM_PER_INCH;
+                double centimeters = inches * cmPerInch;
                 Console.WriteLine("{0} inches is {1} centimeters.", inches, centimeters);
             }
             else
